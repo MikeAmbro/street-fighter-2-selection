865d6cc410b18e50c61472e0f300cf8b4cb5f683
MikeAmbro <126194057+MikeAmbro@users.noreply.github.com>
2023-02-23 13:53:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ document.addEventListener("keydown", (x) => {
     if (position == 5) {
       squares[position].removeChild(document.querySelector(".selected"));
       position = 0;
-      charSelected.src = "images/ambro pixel.png";
+      charSelected.src = "images/char-ambro.png";
       dynamicChange();
       return;
     }
